6381d02a0cd90f079bc256caded3a9e27a52b443
OpeOginni <OpeOginni@users.noreply.github.com>
2025-12-06 19:04:52 +0000

    test commit message

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,5 +2,6 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
+  // Merging classes with the power of tailwind
   return twMerge(clsx(inputs))
 }
