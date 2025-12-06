8f41aa5f32745e111dad1709294d03b37b8ff5d8
OpeOginni <OpeOginni@users.noreply.github.com>
2025-12-06 23:34:45 +0000

    Test 5

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,5 +3,6 @@ import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   // Merging classes with the power of tailwind
+  // This utility combines clsx and tailwind-merge for optimal class handling
   return twMerge(clsx(inputs))
 }
